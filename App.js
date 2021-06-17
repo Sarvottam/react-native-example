6e3c1191b512800a473bcee7f17c7193d90f2833
Sarvottam <sarvottamptl@gmail.com>
2021-06-17 22:20:02 +0530

    navigation for going to first page completed

--- App.js
+++ App.js
@@ -22,8 +22,14 @@ function DetailsScreen({navigation}) {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
       <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Home')}
+        title="Go to Details again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
       />
     </View>
   );
